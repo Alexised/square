c1728bb8b66198272b2f815bea0dc625e28fa223
Alexised <edwinalexis1996@hotmail.com>
2019-05-21 18:16:56 -0500

    First commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'square';
+  lugares:any =[
+    {active:true,nombre:'Floreria'},
+    {active:true,nombre:'Donas'},
+    {active:true,nombre:'Veterinaria'},
+    {active:false,nombre:'sushi'},
+    {active:true,nombre:'hotel'},
+    {active:false,nombre:'zapateria'}
+];
+
+  constructor(){
+
+  }
+
+}
