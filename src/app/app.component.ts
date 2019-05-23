1a7230508ba7451426dc079ab92bfa4364e74d76
Alexised <edwinalexis1996@hotmail.com>
2019-05-23 12:16:14 -0500

    Directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,12 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'square';
   lugares:any =[
-    {cercania:1, distancia:1, active:true,nombre:'Floreria'},
-    {cercania:1, distancia:1.8, active:true,nombre:'Donas'},
-    {cercania:2, distancia:5, active:true,nombre:'Veterinaria'},
-    {cercania:3, distancia:10, active:false,nombre:'sushi'},
-    {cercania:3, distancia:35, active:true,nombre:'hotel'},
-    {cercania:3, distancia:120, active:false,nombre:'zapateria'}
+    {plan: 'pagado',cercania:1, distancia:1, active:true,nombre:'Floreria'},
+    {plan: 'gratuito',cercania:1, distancia:1.8, active:true,nombre:'Donas'},
+    {plan: 'gratuito',cercania:2, distancia:5, active:true,nombre:'Veterinaria'},
+    {plan: 'gratuito',cercania:3, distancia:10, active:false,nombre:'sushi'},
+    {plan: 'pagado',cercania:3, distancia:35, active:true,nombre:'hotel'},
+    {plan: 'gratuito',cercania:3, distancia:120, active:false,nombre:'zapateria'}
 ];
 lat: number = 6.26463;
 lng: number = -75.5687959;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AgmCoreModule } from '@agm/core';
+import {ResaltarDirective} from "./directives/resaltar.directive"
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResaltarDirective
   ],
   imports: [
     BrowserModule,
