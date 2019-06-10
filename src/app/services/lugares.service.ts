7b9e407de607466256d0abcb533be8dc440d4f43
Alexis Rodriguez <edwinalexis1996@hotmail.com>
2019-06-10 17:22:09 -0500

    HTTP

--- src/app/services/lugares.service.ts
+++ src/app/services/lugares.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient ,HttpHeaders } from '@angular/common/http';
 import { AngularFireDatabase } from '@angular/fire/database';
 @Injectable()
 export class LugaresService {
+    API_ENDPOINT = "https://square-241401.firebaseio.com";
     lugares: any = [
         { id: 1, plan: 'pagado', cercania: 1, distancia: 1, active: true, nombre: 'Floreria', description: 'Descripcion de este negocio. Mas adelante tendremos mas informacion' },
         { id: 2, plan: 'gratuito', cercania: 1, distancia: 1.8, active: true, nombre: 'Donas', description: 'Descripcion de este negocio. Mas adelante tendremos mas informacion' },
@@ -11,7 +12,7 @@ export class LugaresService {
         { id: 5, plan: 'pagado', cercania: 3, distancia: 35, active: true, nombre: 'hotel', description: 'Descripcion de este negocio. Mas adelante tendremos mas informacion' },
         { id: 6, plan: 'gratuito', cercania: 3, distancia: 120, active: false, nombre: 'zapateria', description: 'Descripcion de este negocio. Mas adelante tendremos mas informacion' }
     ];
-    constructor(private afDB: AngularFireDatabase, private http: HttpClient ) { }
+    constructor(private afDB: AngularFireDatabase, private http: HttpClient) { }
     public getLugares() {
         return this.afDB.list('lugares/').valueChanges();
     }
@@ -22,15 +23,17 @@ export class LugaresService {
         })[0] || null;
     }
     public guardarLugar(lugar) {
-        this.afDB.database.ref('lugares/' + lugar.id).set(lugar);
-    }
+        // this.afDB.database.ref('lugares/' + lugar.id).set(lugar);
+        const headers = { "Content-Type": "application/json" };
+        return this.http.post(this.API_ENDPOINT+'/lugares.json', lugar ,{headers:headers});
+    }   
     public editarLugar(lugar) {
         this.afDB.database.ref('lugares/' + lugar.id).set(lugar);
     }
     public obtenerGeodata(dirrecion) {
-        return this.http.get('https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyAL5DbMR58gvTncO4ZdsUveJGIqLWzlGas&address='+ dirrecion);
+        return this.http.get('https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyAL5DbMR58gvTncO4ZdsUveJGIqLWzlGas&address=' + dirrecion);
     }
     public getLugar(id) {
-    return this.afDB.object('lugares/' + id).valueChanges();
+        return this.afDB.object('lugares/' + id).valueChanges();
     }
 }
